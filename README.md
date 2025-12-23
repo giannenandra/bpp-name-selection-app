085f4b273e648bff655f0ad88be524c10d5e480d
giannenandra <giannesnandra@gmail.com>
2025-12-23 10:24:37 +0000

    removing files

--- README.md
+++
@@ -1,3 +0,0 @@
-## Name Selection App
-
-This is a name selection app that has been deployed to AWS using Terraform to create the relevant resources and GitHub actions to handle CI/CD Pipelines. 
--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-988c3253" 
-    key            = "name-selection-app/terraform.tfstate"
-    region         = "eu-west-2"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
